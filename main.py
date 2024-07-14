65094f254869d355248023ef10d2bc6bdd58e306
wilson <xiaowenchaode@qq.com>
2024-07-14 21:53:47 +0800

    beta-0.9.1 commit： - 使用PySide6重写界面 - 复现alpha-0.8.1的所有功能

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
-from StreamControl import StreamControl
-from PyQt5.QtWidgets import QApplication
+from StreamPlayer.StreamControl import StreamControl
+from PySide6.QtWidgets import QApplication
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     player = StreamControl()
     player.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
